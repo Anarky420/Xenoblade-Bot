8ee58dd7acae4f85cbca064d0dd5c7d36741b2fa
Anarky420 <39548876+Anarky420@users.noreply.github.com>
2018-05-28 21:35:03 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
 client.on("message", (message) => {
   if(message.content === 'Dunban Talent Art') {
-    message.channel.send('Born In A World Of Strife, Against The Odds, We Choose To Fight, Blossom Dance!.');
+    message.reply('Born In A World Of Strife, Against The Odds, We Choose To Fight, Blossom Dance!.');
   }
   if(message.content === 'Shulk Talent Art') {
-    message.channel.send('Monado Lend Us Your Power');
+    message.reply('Monado Lend Us Your Power');
   }
   if(message.content === 'Melia Talent Art') {
-    message.channel.send('Manifest Yourself');
+    message.reply('Manifest Yourself');
   }
   if(message.content === 'Sharla Talent Art') {
-    message.channel.send('Gotta Cool Off);
+    message.reply('Gotta Cool Off);
   }
   if(message.content === 'Fiora Talent Art') {
-    message.channel.send('Sword Drones!');
+    message.reply('Sword Drones!');
   }
 if(message.content === 'Riki Talent Art') {
-    message.channel.send('Riki Find Goodies');
+    message.reply('Riki Find Goodies');
   }
 });
 
