0cf32607e601fdeef82ef3a11ad7fe6660d0d0ff
Anarky420 <39548876+Anarky420@users.noreply.github.com>
2018-05-29 14:08:33 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,5 +41,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'Hello') {
+    	message.reply('Hello (:');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
