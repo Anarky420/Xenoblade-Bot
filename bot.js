a7fd5c1a1b46675880a0121b69fae625e6e8605b
Anarky420 <39548876+Anarky420@users.noreply.github.com>
2018-05-29 11:30:20 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'Dunban Talent Art') {
+    	message.reply('Born in a world of strife, Against the odds, We choose to fight, Blossom Dance!');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
