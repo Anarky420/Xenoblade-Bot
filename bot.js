125a597d0eb1b50d25ebd68675f15ca66b2498c5
Anarky420 <39548876+Anarky420@users.noreply.github.com>
2018-05-29 18:46:18 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,49 +7,49 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'What do you need to make a rainbow?') {
-    	message.reply('You cant have a rainbow without Reyn Baby!');
+    	message.channel.send('You cant have a rainbow without Reyn Baby!');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'Dunban Talent Art') {
-    	message.reply('Born in a world of strife, Against the odds, We choose to fight, Blossom Dance!');
+    	message.channel.send('Born in a world of strife, Against the odds, We choose to fight, Blossom Dance!');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'Shulk are you feeling it?') {
-    	message.reply('Im Really Feeling it!');
+    	message.channel.send('Im Really Feeling it!');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'Tora what are you doing?') {
-    	message.reply('Tora was just resting his eyes');
+    	message.channel.send('Tora was just resting his eyes');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'Help') {
-    	message.reply('If you need a list of commands, DM @Anarky420#3445');
+    	message.channel.send('If you need a list of commands, DM @Anarky420#3445');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'The Zekenator') {
-    	message.reply('Falls...');
+    	message.channel.send('Falls...');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'Hello') {
-    	message.reply('Hello (:');
+    	message.channel.send('Hello (:');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'Poor Reyn') {
-    	message.reply('Reyn Smelly Orluga');
+    	message.channel.send('Reyn Smelly Orluga');
   	}
 });
 
