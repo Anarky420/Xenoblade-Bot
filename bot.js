e29d7bfa2e7ad667b6b0a88748d65058d9deeb85
Anarky420 <39548876+Anarky420@users.noreply.github.com>
2018-05-29 14:05:18 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,5 +35,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'The Zekenator') {
+    	message.reply('Falls...');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
