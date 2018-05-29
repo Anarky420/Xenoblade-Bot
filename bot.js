d3edbff8ad23964ef5cce8174bf5e94a779f2ba2
Anarky420 <39548876+Anarky420@users.noreply.github.com>
2018-05-29 11:37:38 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,5 +29,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'Help') {
+    	message.reply('If you need a list of commands, DM @Anarky420#3445');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
